45d50c8e8c68da05d34f9edb672e3dd12d99b655
Ankil Mehta <ankilmehta19@gmail.com>
2020-06-08 15:58:46 -0500

    Refactoring while loop to remove boolean variable

--- Bikeshare.py
+++ Bikeshare.py
@@ -24,9 +24,10 @@ def get_filters():
     cities = ['chicago','new york city','washington']
     months = ['all','january','february','march','april','june']
     days = ['all','monday','tuesday','wednesday','thursday','friday','saturday','sunday']
-    while(bool == True):
-        city = input("Enter a city name from chicago, new york city or washington \n")
-        city = city.lower()
+    city = input("Enter a city name from chicago, new york city or washington \n")
+    city = city.lower()
+        
+        while True:
         if(city not in cities):
           print("Please enter city name appropriately from the list \n") 
           
@@ -34,20 +35,24 @@ def get_filters():
             break
 
         # TO DO: get user input for month (all, january, february, ... , june)
-    while(bool == True):
-        month = input("Do you need data for specific month or all? If specific month then please mention the complete name of month ")
+        
+    month = input("Do you need data for specific month or all? If specific month then please mention the complete name of month ")
+    month = month.lower()
+        while True:
         if(month not in months):
-          print("Please enter month name appropriately \n") 
+          print("Please enter the month name appropriately \n") 
         else:
           break
         
         # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
-    while(bool == True):
-        day = input("Do you need data for specific weekday or all? If specific week then please mention the complete weekname of month ")
+    
+    day = input("Do you need data for specific weekday or all? If specific week then please mention the complete weekname of month ")
+    day = day.lower()
+        while True
         if(day not in days):
-          print("Please enter week name appropriately \n") 
+          print("Please enter the week name appropriately \n") 
         else:
-            bool = False
+            break
             
             
     print('-'*40)
